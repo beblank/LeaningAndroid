529755edfa68d3ed7793a18a809f3513cb190683
adit-PC\adit <kulik2@ymail.com>
2017-01-01 20:13:49 +0700

    Dagger 2 Example: creating di interfaces

---
+++ Dagger2Example/app/src/main/java/com/example/adit/dagger2example/di/PerActivity.java
@@ -0,0 +1,14 @@
+package com.example.adit.dagger2example.di;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
+
+/**
+ * Created by adit on 1/1/2017.
+ */
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PerActivity {
+}
