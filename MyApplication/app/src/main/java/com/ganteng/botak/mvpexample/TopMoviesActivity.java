6b60899650da5df33a71ccc1c405d098c7862428
Aditya Hadiwijaya <kulik2@ymail.com>
2017-01-03 15:24:41 +0700

    new project on map example with updated dependencies

---
+++ MyApplication/app/src/main/java/com/ganteng/botak/mvpexample/TopMoviesActivity.java
@@ -0,0 +1,13 @@
+package com.ganteng.botak.mvpexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopMoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_movies);
+    }
+}
