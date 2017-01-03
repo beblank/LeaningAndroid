5311bb03cf1e01222c12373b9191709d05f30725
Aditya Hadiwijaya <kulik2@ymail.com>
2017-01-03 11:18:15 +0700

    Retrofit Example: update Twitch API using observable

--- RetrofitExample/app/src/main/java/com/ganteng/botak/retrofitexample/http/TwitchAPI.java
+++ RetrofitExample/app/src/main/java/com/ganteng/botak/retrofitexample/http/TwitchAPI.java
@@ -5,6 +5,7 @@ import com.ganteng.botak.retrofitexample.http.apimodel.Twitch;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import rx.Observable;
 
 /**
  * Created by adityahadiwijaya on 12/30/16.
@@ -14,4 +15,7 @@ public interface TwitchAPI {
 
     @GET("games/top")
     Call<Twitch> getTopGames(@Header("Client-Id")String clientID);
+
+    @GET("games/top")
+    Observable<Twitch> getTopGamesObservable(@Header("Cllient-Id") String clientId);
 }
