9be461c94f65b0a3f430153a037f9d8fd3244719
Aditya Hadiwijaya <kulik2@ymail.com>
2017-01-03 11:56:32 +0700

    Retrofit Example : update for rxjava top game observable

--- RetrofitExample/app/src/main/java/com/ganteng/botak/retrofitexample/MainActivity.java
+++ RetrofitExample/app/src/main/java/com/ganteng/botak/retrofitexample/MainActivity.java
@@ -17,11 +17,19 @@ import javax.inject.Inject;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     @Inject
     TwitchAPI twitchAPI;
+
+    private static final String TWITCH_API = "vfpywe7ol46tzy6ps15lyv5br6rb4w";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         ((App)getApplication()).getComponent().inject(this);
 
-        Call<Twitch> call = twitchAPI.getTopGames("vfpywe7ol46tzy6ps15lyv5br6rb4w");
+        Call<Twitch> call = twitchAPI.getTopGames(TWITCH_API);
 
         call.enqueue(new Callback<Twitch>() {
             @Override
@@ -47,5 +55,63 @@ public class MainActivity extends AppCompatActivity {
                 t.printStackTrace();
             }
         });
+
+        twitchAPI.getTopGamesObservable(TWITCH_API)
+                .flatMap(flatMapTwitchTop)
+                .flatMap(flatMapTwitchGameName)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(printAction, logOnError, logOnComplete);
+
     }
+
+    Func1<Twitch, Observable<Top>> flatMapTwitchTop = new Func1<Twitch, Observable<Top>>() {
+        @Override
+        public Observable<Top> call(Twitch twitch) {
+            return Observable.from(twitch.getTop());
+        }
+    };
+
+    Func1<Top, Observable<String>> flatMapTwitchGameName = new Func1<Top, Observable<String>>() {
+        @Override
+        public Observable<String> call(Top top) {
+            return Observable.just(top.getGame().getName());
+        }
+    };
+
+    Func1<String, Boolean> filterGameName = new Func1<String, Boolean>() {
+        @Override
+        public Boolean call(String s) {
+            return s.startsWith("H");
+        }
+    };
+
+    Action1<String> printActionWithFilter = new Action1<String>() {
+        @Override
+        public void call(String s) {
+            System.out.println("From rx java with filter" + s);
+        }
+    };
+
+    Action1<String> printAction = new Action1<String>() {
+        @Override
+        public void call(String s) {
+            System.out.print("From rx java" + s);
+        }
+    };
+
+    Action1<Throwable> logOnError = new Action1<Throwable>() {
+        @Override
+        public void call(Throwable throwable) {
+            Log.e("Dodol", "call: " + throwable.getMessage());
+        }
+    };
+
+    Action0 logOnComplete = new Action0() {
+        @Override
+        public void call() {
+            Log.d("dodol", "call: complete action");
+        }
+    };
+
 }
