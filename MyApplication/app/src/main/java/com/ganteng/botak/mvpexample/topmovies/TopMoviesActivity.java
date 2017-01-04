2fa7e8eb38982150b8a90dc45ae0fecef9f6e373
Aditya Hadiwijaya <kulik2@ymail.com>
2017-01-04 11:14:25 +0700

    refactor activity

--- MyApplication/app/src/main/java/com/ganteng/botak/mvpexample/topmovies/TopMoviesActivity.java
+++ MyApplication/app/src/main/java/com/ganteng/botak/mvpexample/topmovies/TopMoviesActivity.java
@@ -1,7 +1,9 @@
-package com.ganteng.botak.mvpexample;
+package com.ganteng.botak.mvpexample.topmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ganteng.botak.mvpexample.R;
 
 public class TopMoviesActivity extends AppCompatActivity {
 
