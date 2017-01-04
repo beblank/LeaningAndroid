a8635609e6562742ba50d46ec4b4d05d8f31161c
Aditya Hadiwijaya <kulik2@ymail.com>
2017-01-04 11:21:07 +0700

    dagger root boiler plate

--- MyApplication/app/src/main/java/com/ganteng/botak/mvpexample/root/App.java
+++ MyApplication/app/src/main/java/com/ganteng/botak/mvpexample/root/App.java
@@ -1,8 +1,27 @@
 package com.ganteng.botak.mvpexample.root;
 
+import android.app.Application;
+
 /**
  * Created by adityahadiwijaya on 1/4/17.
  */
 
-public class App {
+
+
+public class App extends Application{
+
+    private ApplicationComponent component;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        component = DaggerApplicationComponenent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .build();
+    }
+
+    public ApplicationComponent getComponent(){
+        return component;
+    }
 }
--- MyApplication/app/src/main/java/com/ganteng/botak/mvpexample/root/ApplicationModule.java
+++ MyApplication/app/src/main/java/com/ganteng/botak/mvpexample/root/ApplicationModule.java
@@ -1,8 +1,30 @@
 package com.ganteng.botak.mvpexample.root;
 
+import android.app.Application;
+import android.content.Context;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
 /**
  * Created by adityahadiwijaya on 1/4/17.
  */
 
+@Module
 public class ApplicationModule {
+
+    private Application application;
+
+    public ApplicationModule(Application application){
+        this.application = application;
+    }
+
+    @Provides
+    @Singleton
+    public Context provideContext(){
+        return application;
+    }
+
 }
